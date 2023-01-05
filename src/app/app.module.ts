3966bba191475bdbb57c4f52d51a62e73722e6bc
Sebastian Gonzalez Romero <sebasgoro41@gmail.com>
2023-01-05 08:46:49 -0500

    Inicialización del proyecto con rutas hijas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
+// Import angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Import components propios
 import { AppComponent } from './app.component';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+// Import modulos propios
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
