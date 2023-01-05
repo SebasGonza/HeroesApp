210475ef262890510545cb99bc668b621b4af400
Sebastian Gonzalez Romero <sebasgoro41@gmail.com>
2023-01-05 12:16:16 -0500

    cambios en la configuracion

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,16 @@
+// Modulos
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AgregarComponent } from './pages/agregar/agregar.component';
-import { BuscarComponent } from './pages/buscar/buscar.component';
-import { HeroeComponent } from './pages/heroe/heroe.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ListadoComponent } from './pages/listado/listado.component';
+import { MaterialModule } from '../material/material.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from "@angular/flex-layout";  // Importacion del Angular-Flex
+
+// Componentes
+import { HomeComponent } from './pages/home/home.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { ListadoComponent } from './pages/listado/listado.component';
 
 
 
@@ -19,7 +24,9 @@ import { HeroesRoutingModule } from './heroes-routing.module';
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
+    HeroesRoutingModule
   ]
 })
 export class HeroesModule { }
